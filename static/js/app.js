f29e24fe5525aa2f3d83a7cb22f2228d167a644f
Phillee58 <53276449+Phillee58@users.noreply.github.com>
2020-01-17 11:33:11 -0800

    Better

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,17 @@
-// from data.js
+// Main data source
 var tableData = data;
 
-// Select the button
-var button = d3.select("#filter-btn");
+// Filter the table with the FILTER TABLE button
+var buttonFind = d3.select("#filter-btn");
 
 // Complete the click handler for the form
-button.on("click", function() {
-    
+buttonFind.on("click", function() {
+  
   // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
   
   // Get the value property of the input element
     var inputValue = inputElement.property("value");
-    
     console.log(inputValue);
     console.log(tableData);
 
@@ -20,11 +19,9 @@ button.on("click", function() {
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
     console.log(filteredData);
 
-
-// Display the Data in the Table
-// Get a reference to the table body and show filtered data
-var tbody = d3.select("tbody");
-console.log(filteredData);
+  // Display the filtered data in the table body
+    var tbody = d3.select("tbody");
+    console.log(filteredData);
 
 // Fill the table with the Filtered data
 filteredData.forEach((filteredData) => {
@@ -35,3 +32,12 @@ filteredData.forEach((filteredData) => {
   });
 });
 });
+
+// Clear the table with the CLEAR TABLE button
+var buttonClear = d3.select("#clear-btn");
+buttonClear.on("click", function() {
+  d3.select("tbody")
+    .selectAll("tr").remove()
+    .selectAll("td").remove()
+});
+
